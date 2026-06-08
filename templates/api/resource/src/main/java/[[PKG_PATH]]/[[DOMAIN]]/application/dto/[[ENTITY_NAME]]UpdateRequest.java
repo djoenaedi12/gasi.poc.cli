a2aae110e760e5e0e188cfd42003fde7850ab06a
djoenaedi12 <djoenaedi12@gmail.com>
2026-06-08 10:21:25 +0700

    update

--- templates/api/resource/src/main/java/[[PKG_PATH]]/[[DOMAIN]]/application/dto/[[ENTITY_NAME]]UpdateRequest.java
+++ templates/api/resource/src/main/java/[[PKG_PATH]]/[[DOMAIN]]/application/dto/[[ENTITY_NAME]]UpdateRequest.java
@@ -1,6 +1,8 @@
 package {{FULL_PACKAGE}}.application.dto;
 
 {{UPDATE_REQUEST_IMPORTS}}
+import gasi.gps.core.api.application.dto.VersionedRequest;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class {{ENTITY_NAME}}UpdateRequest {
+public class {{ENTITY_NAME}}UpdateRequest implements VersionedRequest {
+    
+    @NotNull
+    private Integer version;
 
 {{UPDATE_REQUEST_FIELDS}}
 }
